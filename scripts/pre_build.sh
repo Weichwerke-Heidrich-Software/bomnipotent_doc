bae1bb0b51ff9c552889ee356a09e90604494e1e
Simon Heidrich <info@wwh-soft.com>
2025-07-17 16:56:47 +0200

    Updating theme.

--- scripts/pre_build.sh
+++ scripts/pre_build.sh
@@ -4,6 +4,11 @@ set -e
 
 cd "$(git rev-parse --show-toplevel)"
 
+cd themes/relearn
+git checkout main
+git pull
+cd -
+
 ./scripts/make_consistent.sh
 ./scripts/generate_short_commands.sh
 ./scripts/delete_empty_outputs.sh
